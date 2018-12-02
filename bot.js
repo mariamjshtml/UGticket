d1e8bf47c8a998dd11460ad34e9878c8d29a3d92
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2018-12-02 12:48:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ function clean(text) {
         return text;
 }
 
-const prefix = "Your Prefix";
-const token = "Token Your Bot";
+const prefix = "-";
+const token = "NTE4NzUyOTcyNDYxODM0MjQx.DuVWUw.8pbfdajXrU2sBbp0epUtRErbQDU";
 
 client.on("ready", () => {
   console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
