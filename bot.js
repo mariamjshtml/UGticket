5169e17ed81eafdb93db03bce05d911fd5ae53bc
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2020-05-10 22:27:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,11 @@ function clean(text) {
         return text;
 }
 
-const prefix = "#";
-const token = "NTE4NzUyOTcyNDYxODM0MjQx.DuVWUw.8pbfdajXrU2sBbp0epUtRErbQDU";
+const prefix = "-";
+const token = "NzA5MTM0ODEzMTAxMTYyNjM2.XrhhUQ.d-GfzhsDCp91JznNR4x2XkgAbNM";
 
 client.on("ready", () => {
-  console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
+  console.log("Exloved | Logged in! Server count: ${client.guilds.size}");
   client.user.setGame(`Support Magic |${prefix}new`);
 });
 
@@ -24,7 +24,7 @@ client.on("message", (message) => {
     const embed = new Discord.RichEmbed()
     .setTitle(`:mailbox_with_mail: Vulnix Help`)
     .setColor(0xCF40FA)
-    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
+    .setDescription(`Hello! I'm Exloved, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
     .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
     .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
     message.channel.send({ embed: embed });
